1554ff3a5cb26265638a4d7e0341f0d35294c080
pablo001p <s469614839@gmail.com>
2025-09-27 21:45:55 +0500

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-900">
+      <h1 className="text-4xl font-bold mb-4">Care-Fin Classic</h1>
+      <p className="text-lg">Добро пожаловать на наш сайт!</p>
+    </div>
+  )
+}
